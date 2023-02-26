b23fb77076e7dc676899affd55eb7ca3dd87f4b3
muhsaputra <muhsaputrabiz@gmail.com>
2023-02-26 16:03:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'index_portfolio.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bg-main': "url('dist/img/main_bg.jpg')",
+      },
+      fontFamily: {
+        'poppins': ['Poppins']
+      },  
+    },
+  },
+  plugins: [],
+}
